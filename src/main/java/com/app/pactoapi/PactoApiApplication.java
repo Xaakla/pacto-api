5c28a459122136f1bd2a981bef3a306ebce8a54b
xaakla <diego.rb0307@gmail.com>
2024-09-20 11:23:46 -0300

    initial commit

---
+++ src/main/java/com/app/pactoapi/PactoApiApplication.java
@@ -0,0 +1,13 @@
+package com.app.pactoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PactoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PactoApiApplication.class, args);
+    }
+
+}
